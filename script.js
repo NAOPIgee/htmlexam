acb60523cefc4e6555b38dc603336987b39bfb1c
Fiorainnnn <11046029@ntub.edu.tw>
2023-01-09 21:39:45 +0800

    新增 - 測試json格式

--- script.js
+++ script.js
@@ -2,19 +2,6 @@ const gamelogoBtn = document.querySelector('.gamelogo');
 const gamelogoShow = document.querySelector('.gamelogo-show');
 const btn = document.querySelector('.btn');
 const menu = document.querySelector('.menu');
-import { myDeck } from 'my-deck.json';
-
-const { DeckEncoder } = require('runeterra');
-
-for (i = 0; i < 12; i++) {
-    const deck = DeckEncoder.decode(myDeck[i].code);
-    deck.code;
-    deck.count;
-    deck.set;
-    deck.id;
-    deck.faction;
-    console.log(deck);
-}
 
 gamelogoBtn.addEventListener("click", () => {
     const visibility = gamelogoShow.getAttribute("data-visible");
